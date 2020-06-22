a402216e200c81cc5d6a9953b3c03640d769513b
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-06-22 21:43:03 +0200

    add emailvalidator and bcrypt

--- server/app/controllers/auth.js
+++ server/app/controllers/auth.js
@@ -1,1 +1,7 @@
 const User = require("../models/user");
+
+
+// une libraire pour tester le format des email
+const emailValidator = require("email-validator");
+// bcrypt, pour HASHER les mots de passer
+const bcrypt = require("bcrypt");
