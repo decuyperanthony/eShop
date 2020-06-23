9fae2b9df5ebfa0a08625f2f3e670db560fc4204
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-06-23 02:01:09 +0200

    add route for authController

--- server/app/router.js
+++ server/app/router.js
@@ -1,5 +1,15 @@
-// Require express
+// == Require express
 const express = require('express');
+
+// == Controller
+const authController = require('./controllers/authController');
+
 const router = express.Router();
+
+
+// == page login
+router.post("/login", authController.loginAction);
+
+
 
 module.exports = router;
