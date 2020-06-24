1689bcfc95a05c6e9fa0bebf4fd8c754f7782a39
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-06-24 19:06:49 +0200

    add association between article user rating and comment

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -2,6 +2,8 @@ const User = require('./user');
 const Article = require('./article');
 const Category = require('./category');
 const Collection = require('./collection');
+const Rating = require('./rating');
+const Comment = require('./comment');
 
 
 
@@ -11,11 +13,11 @@ Article.belongsTo(Category, {
     as: "category"
 })
 
-// Category.hasMany(Article)
 Category.hasMany(Article, {
     foreignKey: "category_id",
     as: "articles"
 })
+
 
 //*    association Article <-> Collection
 Article.belongsTo(Collection, {
@@ -23,12 +25,56 @@ Article.belongsTo(Collection, {
     as: "collection"
 })
 
-// Category.hasMany(Article)
 Collection.hasMany(Article, {
     foreignKey: "collection_id",
     as: "articles"
 })
 
+//! ça doit etre l'inverse à verifier
+//*  -----  association Article <-> Rating
+Article.hasMany(Rating, {
+    foreignKey: "article_id",
+    as: "rates"
+})
+
+Rating.belongsTo(Article, {
+    foreignKey: "article_id",
+    as: "ratingArticle"
+})
+
+
+//* -----   association Article <-> Comment
+Article.hasMany(Comment, {
+    foreignKey: "article_id",
+    as: "comments"
+})
+
+Comment.belongsTo(Article, {
+    foreignKey: "article_id",
+    as: "article"
+})
+
+//*  -----  association User <-> Rating
+User.hasMany(Rating, {
+    foreignKey: "user_id",
+    as: "rates"
+})
+
+Rating.belongsTo(User, {
+    foreignKey: "user_id",
+    as: "author"
+})
+
+//* -----   association User <-> Comment
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    as: "comments"
+})
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    as: "author"
+})
 
 
 
@@ -37,5 +83,7 @@ module.exports = {
     User,
     Article,
     Category,
-    Collection
+    Collection,
+    Comment,
+    Rating
 }
