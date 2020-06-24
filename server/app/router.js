deb008341fccfa37808d5993fd0ed6ad8b8c436b
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-06-24 19:06:58 +0200

    Update router.js

--- server/app/router.js
+++ server/app/router.js
@@ -4,6 +4,7 @@ const express = require('express');
 // == Controller
 const authController = require('./controllers/authController');
 const articleController = require('./controllers/articleController');
+const commentController = require('./controllers/commentController');
 
 
 const router = express.Router();
@@ -21,6 +22,8 @@ router.post("/article", articleController.addArticle);
 router.patch("/article/:id", articleController.updateArticle);
 router.delete("/article/:id", articleController.removeArticle);
 
-
+//* ----- ROUTE DES COMMENTS -----
+router.get("/comment", commentController.getAllComments);
+router.post("/comment/user/:user_id/article/:article_id", commentController.addComment);
 
 module.exports = router;
