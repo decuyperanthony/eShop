eb549f8c63ed9947811009e8b8bd25cc4546c507
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-06-25 10:07:01 +0200

    add and finished ratingController CRUD ok! :+1:

---
+++ server/app/controllers/ratingController.js
@@ -0,0 +1,99 @@
+const { Rating } = require('../models');
+// mettre les include et check le remove and ratingController ok
+const ratingController = {
+    getAllRates: async (req, res) => {
+        try {
+            let rates = await Rating.findAll({
+                // offset,
+                // limit,
+                include: ["author", "ratingArticle"],
+                // order: [[title, 'ASC'], [name, 'ASC']],
+            });
+            res.send(rates);
+        } catch (error) {
+            console.trace(error);
+            res.status(500).send(error);
+        }
+    },
+    getOneRate: async (req, res) => {
+        try {
+            let rating_id = req.params.id;
+            let rate = await Rating.findByPk(rating_id, {
+                include: ["author", "ratingArticle"],
+            });
+            if (!rate) {
+                return res.status(401).send('cette note n\' existe pas')
+            }
+            res.send(rate);
+        } catch (error) {
+            console.trace(error);
+            res.status(500).send(error);
+        }
+    },
+    addRate: async (req, res) => {
+        let { user_id, article_id } = req.params;
+        // let articleId = req.params.article_id;
+        try {
+            const newRate = new Rating(req.body);
+            newRate.user_id = user_id;
+            newRate.article_id = article_id;
+            const savedRate = await newRate.save();
+            res.status(200).send(savedRate);
+        } catch (error) {
+            console.trace(error);
+            res.status(500).send(error);
+        }
+    },
+    updateRate: async (req, res, next) => {
+        let { rating_id, user_id } = req.params;
+
+        try {
+            const rate = await Rating.findByPk(rating_id);
+            if (!rate) {
+                console.log('Cette note n existe pas');
+                return res.status(401).send('Cette note n\'existe pas');
+            }
+
+            if (user_id == rate.user_id) {
+                console.log('ok');
+                // on va pouvoir update
+                // console.log('req.body', req.body);
+                await rate.update(req.body);
+                res.status(200).send(rate);
+            } else {
+                console.log('pas le bon id');
+                return res.status(401).send('vous n\'êtes pas le proprietaire de cette note');
+            }
+        } catch (error) {
+            console.trace(error);
+            res.status(500).send(error);
+        }
+    },
+    removeRate: async (req, res, next) => {
+        // const articleId = req.params.id;
+        let { rating_id, user_id } = req.params;
+
+   try {
+       const rate = await Rating.findByPk(rating_id);
+       if (!rate) {
+           console.log('Cette note n existe pas');
+           return res.status(401).send('Cette note n\'existe pas');
+       }
+       if (user_id == rate.user_id) {
+           console.log('ok');
+           // on va pouvoir supprimer
+           // console.log('req.body', req.body);
+           rate.destroy();
+           res.status(200).send('note supprimée');
+       } else {
+           console.log('pas le bon id');
+           return res.status(401).send('vous n\'êtes pas le proprietaire de cette note');
+       }
+   } catch (error) {
+            console.trace(error);
+            res.status(500).send(error);
+        }
+    }
+}
+
+module.exports = ratingController;
--- server/app/router.js
+++ server/app/router.js
@@ -5,6 +5,9 @@ const express = require('express');
 const authController = require('./controllers/authController');
 const articleController = require('./controllers/articleController');
 const commentController = require('./controllers/commentController');
+const userController = require('./controllers/userController');
+const ratingController = require('./controllers/ratingController');
+
 
 
 const router = express.Router();
@@ -14,6 +17,14 @@ const router = express.Router();
 router.post("/login", authController.loginAction);
 // == page signup
 router.post("/signup", authController.signupAction);
+
+//* ----- ROUTE DES USERS -----
+router.get("/user", userController.getAllUsers);
+router.get("/user/:id", userController.getOneUser);
+// router.post("/user/user/:user_id/article/:article_id", commentController.addComment);
+router.patch("/user/:id", userController.updateUser);
+router.delete("/user/:id", userController.removeUser);
+
 
 //* ----- ROUTE DES ARTICLES -----
 router.get("/article", articleController.getAllArticles);
@@ -30,11 +41,11 @@ router.patch("/comment/:comment_id/user/:user_id", commentController.updateComme
 router.delete("/comment/:comment_id/user/:user_id", commentController.removeComment);
 
 //* ----- ROUTE DES RAITING -----
-// router.get("/comment", commentController.getAllComments);
-// router.get("/comment/:id", commentController.getOneComment);
-// router.post("/comment/user/:user_id/article/:article_id", commentController.addComment);
-// router.patch("/comment/:comment_id/user/:user_id", commentController.updateComment);
-// router.delete("/comment/:comment_id/user/:user_id", commentController.removeComment);
+router.get("/rating", ratingController.getAllRates);
+router.get("/rating/:id", ratingController.getOneRate);
+router.post("/rating/user/:user_id/article/:article_id", ratingController.addRate);
+router.patch("/rating/:rating_id/user/:user_id", ratingController.updateRate);
+router.delete("/rating/:rating_id/user/:user_id", ratingController.removeRate);
 
 
 
