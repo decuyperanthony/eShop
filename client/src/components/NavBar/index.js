39e6e456f36478e71f8f4718fa0b24bac0c33a19
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-06-29 11:56:51 +0200

    change text in navBar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -20,12 +20,12 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <Nav.Link href="#home">Accueil</Nav.Link>
+            <Nav.Link href="#link">Profil</Nav.Link>
+            <NavDropdown title="Articles" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Bracelets</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Collier</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Médailles</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
